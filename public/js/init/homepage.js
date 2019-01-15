1106de001a8e9c3aad5e5f5e85968da5ce51efed
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-15 15:41:25 +0530

    populate machine list

--- public/js/init/homepage.js
+++ public/js/init/homepage.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-    var tabinstance = M.Tabs.init(document.getElementsByClassName('tabs'), {
-        swipeable: true
-    });
+$(document).ready(function () {
+    var tabinstances = M.Tabs.init(document.getElementsByClassName('tabs'));
+    // var machinemodalinstance = M.Modal.init(document.getElementById('machine-modal'));
+
+    $('.modal').modal();
+
+    $.get('/machines/list', function(listmachines){
+        var machineview = new Vue({
+            el: '#machines',
+            data: {
+                machines: listmachines
+            }
+        })
+    })
+    
 });
