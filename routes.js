2e375636b34331dbd6a48d6d1e6c87cf481f74dd
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-15 15:38:41 +0530

    machine routes

--- routes.js
+++ routes.js
@@ -7,4 +7,17 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+
+// Machine -----
+
+var machineController = require('./controllers/machineController')
+
+router.get('/machines', machineController.machines_get)
+
+router.get('/machines/list', machineController.machines_list_get)
+
+router.get('/machine/:id', machineController.machine_detail_get)
+
+router.post('/machine/create', machineController.machine_create_post)
+
 module.exports = router
