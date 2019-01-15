ad907609832abc7b70ef20d33a7a59db852f61d9
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-15 15:38:24 +0530

    setup mongoose and bodyprser

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
+const DBPORT = process.env.MONGODB_URI || 'mongodb://heroku_glpd1cfw:p4g2fbe0l74homdtpals7rg0cm@ds157574.mlab.com:57574/heroku_glpd1cfw'
 
 var app = express()
 
+mongoose.connect(DBPORT)
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+
 app.use(express.static('public'))
+app.use(express.static('public/build/html'))
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -12,9 +30,9 @@ app.set('views', './views');
 
 app.use('/', require('./routes'))
 
-app.listen(port, err => {
+app.listen(PORT, err => {
     if (err) {
         throw err
     }
-    console.info('Listening on port ' + port + "...")
+    console.info('Listening on port ' + PORT + "...")
 })
