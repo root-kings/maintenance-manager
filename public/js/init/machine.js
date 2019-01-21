f7972f71adf2f2e06fe1ba636da23c74f3289106
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-22 00:12:23 +0530

    add record handler. isme jaan nikal gyi meri

--- public/js/init/machine.js
+++ public/js/init/machine.js
@@ -1,28 +1,46 @@
 var machineview;
 
 $(function () {
-    M.Datepicker.init(document.getElementById('date'), {
-        defaultDate: new Date(),
-        setDefaultDate: true
-    });
-
-    M.updateTextFields();
-
     populate(localStorage.getItem('machineId'));
 })
 
 function populate(id) {
     $.get('/api/machine/' + id, function (machine) {
+        machine.checkup.history.sort().reverse();
+
+        // machine.checkup.last = moment(this.checkup.history.sort()[0]).format("DD MMM YYYY");
+
         machineview = new Vue({
             el: '#machine',
             data: {
                 machine: machine
+            },
+            mounted: function () {
+                M.Datepicker.init(document.getElementById('date'), {
+                    defaultDate: new Date(),
+                    setDefaultDate: true,
+                    format: "yyyy-mm-dd"
+                });
+                M.updateTextFields();
+
             }
         })
 
     })
 }
 
-function mark(id) {
-    console.log(id);
+function addRecord(id) {
+    var record = {
+        id: id,
+        date: $("#date").val()
+    }
+
+    console.log(record);
+
+    $.post('/api/machine/record/add', record, function (result) {
+        if (result) {
+            window.location.reload();
+        }
+    })
+
 }
