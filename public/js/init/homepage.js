25fe3beb36689efaeb7537bb88d37d2d71560c1e
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-30 22:51:42 +0530

    soondays calculte aur sorting kiya

--- public/js/init/homepage.js
+++ public/js/init/homepage.js
@@ -3,7 +3,17 @@ var upcomingview;
 $(document).ready(function () {
 
     $.get('/machines/list', function (listmachines) {
-        
+        listmachines.map(function (machine) {
+            machine.soondays = moment(machine.checkup.next).diff(moment(), "days");
+            machine.soon = (machine.soondays <= 10) ? true : false;
+        });
+
+        listmachines.sort(function(m1,m2){
+            if (m1.soondays > m2.soondays) return 1;
+            if (m1.soondays < m2.soondays) return -1;
+            return 0;
+        });
+
         upcomingview = new Vue({
             el: '#upcoming',
             data: {
