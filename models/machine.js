2ece399607ca523da37dd49516ec61c420522575
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-30 22:51:59 +0530

    last ki sorting kiya descending

--- models/machine.js
+++ models/machine.js
@@ -32,7 +32,7 @@ var MachineSchema = new Schema({
         },
         history: [{
             type: Date,
-            
+
         }]
     }
 }, {
@@ -47,7 +47,13 @@ var MachineSchema = new Schema({
 MachineSchema
     .virtual('checkup.last')
     .get(function () {
-        return moment(this.checkup.history.sort()[0]).format("DD MMM YYYY");
+        return moment(this.checkup.history.sort(function (date1, date2) {
+            // This is a comparison function that will result in dates being sorted in
+            // DESCENDING order.
+            if (date1 > date2) return -1;
+            if (date1 < date2) return 1;
+            return 0;
+        })[0]).format("DD MMM YYYY");
     });
 
 MachineSchema
