37abe56ee3d456570a0dcfbcbaee92e3c40e6fa1
krushndayshmookh <krushndayshmookh@gmail.com>
2019-02-03 20:59:10 +0530

    make list collapsible

--- public/js/init/calibration.js
+++ public/js/init/calibration.js
@@ -18,6 +18,19 @@ $(document).ready(function () {
             el: '#upcoming',
             data: {
                 machines: listmachines
+            },
+
+            mounted: function () {
+                M.Collapsible.init(document.querySelectorAll('.collapsible'), {
+                    accordion: false
+                });
+
+                M.Datepicker.init(document.querySelectorAll('.datepicker'), {
+                    defaultDate: new Date(),
+                    setDefaultDate: true,
+                    format: "yyyy-mm-dd"
+                });
+
             }
         })
 
@@ -28,4 +41,28 @@ $(document).ready(function () {
 function viewMachine(id) {
     localStorage.setItem('machineId', id);
     window.location.href = '/machine';
+}
+
+function addRecord(id) {
+    var record = {
+        id: id,
+        date: $("#date" + id).val()
+    }
+
+    console.log(record);
+
+    $.post('/api/machine/record/add', record, function (result) {
+        if (result) {
+            window.location.reload();
+        }
+    })
+
+}
+
+function machineDelete(id) {
+    if (confirm("Delete this machine?")) {
+        $.post('/api/machine/' + id + '/delete', function (result) {
+            window.location = '/machines';
+        });
+    }
 }
