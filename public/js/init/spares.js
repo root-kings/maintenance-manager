56994c58a114166f0dfd87b4ff8de8e6b3331baa
krushndayshmookh <krushndayshmookh@gmail.com>
2019-02-13 11:14:47 +0530

    spareEditTimerModal and remove jquery

--- public/js/init/spares.js
+++ public/js/init/spares.js
@@ -1,8 +1,12 @@
-var spareview, stageEditView;
+var spareview, 
+    stageEditView,
+    stageEditViewModal;
 
-$(document).ready(function () {
-    
-    $.get('/spares/list', function (listspares) {
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/spares/list').then(function(response){
+        return response.json();
+    }).then(function (listspares) {
 
         spareview = new Vue({
             el: '#spares',
@@ -14,16 +18,11 @@ $(document).ready(function () {
                     accordion: false
                 });
                 M.Modal.init(document.querySelectorAll('.modal'));
-                // M.Datepicker.init(document.querySelectorAll('.datepicker'), {
-                //     defaultDate: new Date(),
-                //     setDefaultDate: true,
-                //     format: "yyyy-mm-dd"
-                // });
-                // M.updateTextFields();
             }
         })
 
     })
+
 
 });
 
@@ -35,32 +34,53 @@ function spareEdit(id) {
 
 function spareDelete(id) {
     if (confirm("Delete this spare?")) {
-        $.post('/api/spare/' + id + '/delete', function (result) {
-            window.location = '/spares';
+        fetch('/api/spare/' + id + '/delete',{
+            method: "POST", 
+            mode: "cors", 
+            
+        }).then(function(response){
+            return response.json();
+        }).then(function(result){
+            if (result) {
+                window.location.reload();
+            }
         });
+        /* $.post('/api/spare/' + id + '/delete', function (result) {
+            window.location = '/spares';
+        }); */
     }
 }
 
 function spareStageUpdate(id,stage){
-    $.post('/api/spare/' + id + '/stage',{stage: stage}, function (result) {
-        window.location = '/spares';
-    });
+    // $.post('/api/spare/' + id + '/stage',{stage: stage}, function (result) {
+    //     window.location = '/spares';
+    // });
+    spareEditTimerModal(id);
 }
 
 function spareEditTimerModal(id){
     
-    // var instances = M.Modal.init(document.getElementById('sparestagemodal'));
     
     
-    $.get('/spare/'+id, function (spare) {
-    
+    fetch('/api/spare/'+id).then(function(response){
+        return response.json();
+    }).then(function (spare) {
+
+        spare.requisition.date = moment(spare.requisition.date).format('YYYY-MM-DD');
+        spare.vetting.date = moment(spare.vetting.date).format('YYYY-MM-DD');
+        spare.tod.date = moment(spare.tod.date).format('YYYY-MM-DD');
+        spare.tsc.date = moment(spare.tsc.date).format('YYYY-MM-DD');
+        spare.so.date = moment(spare.so.date).format('YYYY-MM-DD');
+
         stageEditView = new Vue({
             el: '#sparestagemodal',
             data: {
                 spare: spare
             },
             mounted: function () {
-                // instances.open();
+                stageEditViewModal = M.Modal.init(document.getElementById('sparestagemodal'))
+
+                stageEditViewModal.open();
                 
                 M.Datepicker.init(document.querySelectorAll('.datepicker'), {
                     defaultDate: new Date(),
