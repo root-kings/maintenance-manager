f02b6287eb15c5c3bbf3565e3f2727f8f82b25b5
mszire <mrunalzire@gmail.com>
2019-02-16 22:53:01 +0530

    Still it's not working properly

--- mailer.js
+++ mailer.js
@@ -1,1 +1,11 @@
-console.log('Hello! I will mail something.')
+Email.send({
+    Host : "smtp.yourisp.com",
+    Username : "punamkakpure@gmail.com",
+    Password : " 14f059b0-c276-43fc-bcd0-383b440b8e97",
+    To : 'them@website.com',
+    From : "punamkakpure@gmail.com",
+    Subject : "This is the dummy Subject",
+    Body : "We are From OFAJ"
+}).then(
+  message => alert(message)
+);
