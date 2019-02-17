c2fb2661105589883e448817f3a28e338893be67
krushndayshmookh <krushndayshmookh@gmail.com>
2019-02-17 14:19:23 +0530

    setup sendgrid

--- mailer.js
+++ mailer.js
@@ -1,11 +1,24 @@
-Email.send({
-    Host : "smtp.yourisp.com",
-    Username : "punamkakpure@gmail.com",
-    Password : " 14f059b0-c276-43fc-bcd0-383b440b8e97",
-    To : 'them@website.com',
-    From : "punamkakpure@gmail.com",
-    Subject : "This is the dummy Subject",
-    Body : "We are From OFAJ"
-}).then(
-  message => alert(message)
-);
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+	to: [ 'joshi_tushar.ghrcecs@raisoni.net', 'zire_mrunalsingh.ghrcecs@raisoni.net'],
+	from: 'zire@chutya.com',
+	subject: 'Zire Gandu hi rhega saala zindagi bhar',
+	html: '<h1>Subject Dekh le Bhosdike</h1>'
+}
+sgMail
+	.sendMultiple(msg)
+	.then(result => {
+		//Celebrate
+		console.log('Sent mails.')
+	})
+	.catch(error => {
+		//Log friendly error
+		console.error(error.toString())
+
+		//Extract error msg
+		// const { message, code, response } = error
+
+		//Extract response msg
+		// const { headers, body } = response
+	})
