f5fe5af780dc5aab2114bca26b53a643522ea69d
krushndayshmookh <krushndayshmookh@gmail.com>
2019-02-22 10:22:49 +0530

    remove tushar

--- mailer.js
+++ mailer.js
@@ -3,7 +3,7 @@ const msg91 = require('msg91')('247111AI4S9E1P5bea6b3a', 'krushn', '4')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 let msg = {
-	to: ['joshi_tushar.ghrcecs@raisoni.net', 'zire_mrunalsingh.ghrcecs@raisoni.net'],
+	to: [ 'zire_mrunalsingh.ghrcecs@raisoni.net'],
 	from: 'notifications@rootkings.com',
 	subject: 'Fix maintenence manager email and sms notifications.',
 	html: '<h1>Jab tak nai kroge, tab tak ye program pareshan krega.</h1>'
@@ -26,7 +26,7 @@ sgMail
 		// const { headers, body } = response
 	})
 
-let mobileNo = [ '+919021735821', '+918208396310']
+let mobileNo = [  '+918208396310']
 let message = 'Fix maintenence manager email and sms notifications.'
 msg91.send(mobileNo, message, function(err, response) {
 	if (err) console.log(err)
